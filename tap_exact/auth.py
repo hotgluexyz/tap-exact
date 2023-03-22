7c36e5b6e7f1d367fd5c919d04c1f397c6a94b74
Álvaro Valarezo <alvaro@hotglue.io>
2023-03-22 09:48:41 -0500

    fix expires in

--- tap_exact/auth.py
+++ tap_exact/auth.py
@@ -40,7 +40,7 @@ class OAuth2Authenticator(APIAuthenticatorBase):
     def is_token_valid(self) -> bool:
         access_token = self._tap._config.get("access_token")
         now = round(datetime.utcnow().timestamp())
-        expires_in = self._tap.config.get("expires_in")
+        expires_in = int(self._tap.config.get("expires_in"))
 
         if not access_token:
             return False
