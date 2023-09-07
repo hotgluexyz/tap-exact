065c2e76849ad311eae17e3dfb434e22093d8cc2
Key <keynarafaelp@gmail.com>
2023-09-07 16:08:22 -0500

    fix backoff

--- tap_exact/auth.py
+++ tap_exact/auth.py
@@ -52,7 +52,7 @@ class OAuth2Authenticator(APIAuthenticatorBase):
 
         return not ((expires_in - now) < 120)
 
-    @backoff.on_exception(backoff.expo, Exception, max_tries=3,interval = 2)
+    @backoff.on_exception(backoff.expo, Exception, max_tries=3)
     def update_access_token(self) -> None:
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         token_response = requests.post(
